67acc650b0b4be7afa5c935891ddc3c814cf1e54
mikramarc <michal@shadowrobot.com>
2017-06-21 15:07:42 +0100

    Update interface_setup.sh
    slight fix

--- docker/interface_setup.sh
+++ docker/interface_setup.sh
@@ -1,5 +1,8 @@
-source /opt/ros/kinetic/setup.bash &&
-source /home/user/projects/shadow_robot/base_deps/devel/setup.bash &&
-roscd sr_interface && git checkout F#SRC-318/arm_with_box &&
-roscd sr_blockly_blocks && git checkout F#SRC-318/new_blocks &&
+#!/bin/bash
+
+source /opt/ros/kinetic/setup.bash
+source /home/user/projects/shadow_robot/base_deps/devel/setup.bash
+source /home/user/projects/shadow_robot/base/devel/setup.bash
+roscd sr_interface && git checkout F#SRC-318/arm_with_box
+roscd sr_blockly_blocks && git checkout F#SRC-318/new_blocks
 /usr/bin/terminator
