5a790a822aa72d3fe998b85d4708f5973eaf722d
mikramarc <michal@shadowrobot.com>
2017-06-21 14:47:04 +0100

    added interface_setup.sh file

---
+++ docker/interface_setup.sh
@@ -0,0 +1,5 @@
+source /opt/ros/kinetic/setup.bash &&
+source /home/user/projects/shadow_robot/base_deps/devel/setup.bash &&
+roscd sr_interface && git checkout F#SRC-318/arm_with_box &&
+roscd sr_blockly_blocks && git checkout F#SRC-318/new_blocks &&
+/usr/bin/terminator
