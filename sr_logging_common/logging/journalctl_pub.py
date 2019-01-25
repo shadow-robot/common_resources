093533a6970a8629085afe83f9dc49b02a140c8d
azompas <anastasios@shadowrobot.com>
2019-01-25 16:50:40 +0000

    update for journal logging

--- sr_logging_common/logging/journalctl_pub.py
+++ sr_logging_common/logging/journalctl_pub.py
@@ -2,7 +2,6 @@
 
 import rospy
 from std_msgs.msg import String
-import sys
 import subprocess
 import select
 
@@ -11,13 +10,14 @@ if __name__ == "__main__":
     rospy.init_node('journalctl_pub', anonymous=True)
     pub = rospy.Publisher('journalctl_pub', String, queue_size=10)
 
-    args = ['journalctl', '--lines', '0', '--follow', '_SYSTEMD_UNIT=docker.service']
+    args = ['journalctl', '--lines', '0', '--follow']
     f = subprocess.Popen(args, stdout=subprocess.PIPE)
     p = select.poll()
     p.register(f.stdout)
 
     while not rospy.is_shutdown():
         if p.poll(100):
-            line = f.stdout.readline()
-            rospy.loginfo(line.strip())
-            pub.publish(line.strip())
+            log_line = f.stdout.readline()
+            if log_line:
+                rospy.loginfo(log_line.strip())
+                pub.publish(log_line.strip())
