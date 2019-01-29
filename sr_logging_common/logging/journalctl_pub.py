e96768295712a6aff8df4cb265621fd5dcb19689
azompas <anastasios@shadowrobot.com>
2019-01-29 14:09:30 +0000

    custom message for logging added

--- sr_logging_common/logging/journalctl_pub.py
+++ sr_logging_common/logging/journalctl_pub.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from sr_msgs_common.msg import journal_log_msg
 import subprocess
 import select
 
 
 if __name__ == "__main__":
     rospy.init_node('journalctl_pub', anonymous=True)
-    pub = rospy.Publisher('journalctl_pub', String, queue_size=10)
+    pub = rospy.Publisher('journalctl_pub', journal_log_msg, queue_size=10)
 
     args = ['journalctl', '--lines', '0', '--follow']
     f = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -17,7 +17,9 @@ if __name__ == "__main__":
 
     while not rospy.is_shutdown():
         if p.poll(100):
-            log_line = f.stdout.readline()
-            if log_line:
-                rospy.loginfo(log_line.strip())
-                pub.publish(log_line.strip())
+            journal_log_line = f.stdout.readline()
+            if journal_log_line:
+                journal_msg = journal_log_msg()
+                journal_msg.data = journal_log_line.strip()
+                journal_msg.timestamp = rospy.get_rostime()
+                pub.publish(journal_msg)
