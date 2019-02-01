e0b40d7832d17f85d9db4f8622ffe6cf6b800ef4
azompas <anastasios@shadowrobot.com>
2019-02-01 17:42:45 +0000

    rename topic and msg file

--- sr_logging_common/logging/journalctl_pub.py
+++ sr_logging_common/logging/journalctl_pub.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 import rospy
-from sr_msgs_common.msg import journal_log_msg
+from sr_msgs_common.msg import journal_log
 import subprocess
 import select
 
 
 if __name__ == "__main__":
     rospy.init_node('journalctl_pub', anonymous=True)
-    pub = rospy.Publisher('journalctl_pub', journal_log_msg, queue_size=10)
+    pub = rospy.Publisher('journalctl_log', journal_log, queue_size=10)
 
     args = ['journalctl', '--lines', '0', '--follow']
     f = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         if p.poll(100):
             journal_log_line = f.stdout.readline()
             if journal_log_line:
-                journal_msg = journal_log_msg()
+                journal_msg = journal_log()
                 journal_msg.data = journal_log_line.strip()
                 journal_msg.timestamp = rospy.get_rostime()
                 pub.publish(journal_msg)
