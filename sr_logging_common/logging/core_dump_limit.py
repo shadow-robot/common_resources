ec0d8ef46b49941457d88cdd0744b8856044e30e
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2019-02-05 16:14:06 +0000

    Update core_dump_limit.py

--- sr_logging_common/logging/core_dump_limit.py
+++ sr_logging_common/logging/core_dump_limit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# Copyright (C) 2019 Shadow Robot Company Ltd - All Rights Reserved.
+# Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
+
 import os
 import rospy
 import time
