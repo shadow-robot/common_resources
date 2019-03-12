b4e35a42ac6dc4e720781185a67f043689b33f10
mikramarc <michal@shadowrobot.com>
2019-03-12 19:17:28 +0000

    parsing yaml file straight in the node

--- sr_run_trajectories/scripts/run_trajectories_node.py
+++ sr_run_trajectories/scripts/run_trajectories_node.py
@@ -4,12 +4,14 @@
 # Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
 
 import rospy
+import rospkg
 from sr_run_trajectories.run_trajectories import SrRunTrajectories
 
 if __name__ == "__main__":
     rospy.init_node('run_trajectories_node')
+    trajectories_file_path = rospkg.RosPack().get_path('sr_run_trajectories') + '/config/example_trajectories.yaml'
 
-    srt = SrRunTrajectories()
+    srt = SrRunTrajectories(trajectories_file_path)
     srt.run_trajectory('arm', 'test_trajectory')
     srt.run_trajectory('hand', 'test_trajectory')
     srt.run_trajectory('arm_and_hand', 'test_trajectory')
--- sr_run_trajectories/src/sr_run_trajectories/run_trajectories.py
+++ sr_run_trajectories/src/sr_run_trajectories/run_trajectories.py
@@ -3,19 +3,19 @@
 # Copyright (C) 2017 Shadow Robot Company Ltd - All Rights Reserved.
 # Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
 
-
+import rospy
+import yaml
 from sr_robot_commander.sr_arm_commander import SrArmCommander
 from sr_robot_commander.sr_hand_commander import SrHandCommander
-import rospy
 
 class SrRunTrajectories(object):
-    def __init__(self):
+    def __init__(self, trajectories_file_path):
         self.arm_trajectories = {}
         self.hand_trajectories = {}
         self.arm_and_hand_trajectories = {}
 
         self._init_moveit_commanders()
-        self._parse_all_trajectories()
+        self._parse_all_trajectories(trajectories_file_path)
 
     def _init_moveit_commanders(self):
         self.arm_commander = SrArmCommander(name='right_arm_and_wrist', set_ground=False)
@@ -26,14 +26,17 @@ class SrRunTrajectories(object):
         self.arm_commander.set_max_velocity_scaling_factor(0.3)
         self.arm_and_hand_commander.set_max_velocity_scaling_factor(0.1)
 
-    def _parse_all_trajectories(self):
-        arm_joints_order = rospy.get_param("/arm_joints_order")
-        hand_joints_order = rospy.get_param("/hand_joints_order")
-        arm_and_hand_joints_order = rospy.get_param("/arm_and_hand_joints_order")
+    def _parse_all_trajectories(self, trajectories_file_path):
+        with open(trajectories_file_path, 'r') as stream:
+            trajectories_info = yaml.load(stream)
 
-        arm_trajectories = rospy.get_param("/arm_trajectories", {})
-        hand_trajectories = rospy.get_param("/hand_trajectories", {})
-        arm_and_hand_trajectories = rospy.get_param("/arm_and_hand_trajectories", {})
+        arm_joints_order = trajectories_info['arm_joints_order']
+        hand_joints_order = trajectories_info['hand_joints_order']
+        arm_and_hand_joints_order = trajectories_info['arm_and_hand_joints_order']
+
+        arm_trajectories = trajectories_info['arm_trajectories']
+        hand_trajectories = trajectories_info['hand_trajectories']
+        arm_and_hand_trajectories = trajectories_info['arm_and_hand_trajectories']
 
         self.arm_trajectories = self._parse_trajectories_dict(arm_trajectories, arm_joints_order)
         self.hand_trajectories = self._parse_trajectories_dict(hand_trajectories, hand_joints_order)
