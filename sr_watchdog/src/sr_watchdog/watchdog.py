927db3521a588c8cff30058c50c0c723b36ad318
mikramarc <michal@shadowrobot.com>
2019-07-10 12:22:03 +0000

    updating according to review

--- sr_watchdog/src/sr_watchdog/watchdog.py
+++ sr_watchdog/src/sr_watchdog/watchdog.py
@@ -217,24 +217,26 @@ class SrWatchdogChecks(object):
 
     @staticmethod
     def watchdog_error_check(function):
-        wrapper = SrWatchdogChecks.watchdog_check(function)
+        wrapper = SrWatchdogChecks.watchdog_check_decorator(function)
         wrapper.check_type = CheckStatus.ERROR
         return wrapper
 
     @staticmethod
     def watchdog_warning_check(function):
-        wrapper = SrWatchdogChecks.watchdog_check(function)
+        wrapper = SrWatchdogChecks.watchdog_check_decorator(function)
         wrapper.check_type = CheckStatus.WARN
         return wrapper
 
     @staticmethod
-    def watchdog_check(function):
+    def watchdog_check_decorator(function):
         def wrapper(self):
+            CONST_RETURN_TUPLE_EXPECTED_SIZE = 2
             return_value = function(self)
             if isinstance(return_value, bool):
                 result = return_value
                 error_msg = None
-            elif isinstance(return_value, tuple) and 2 == len(return_value):
+            elif isinstance(return_value, tuple) and \
+                 CONST_RETURN_TUPLE_EXPECTED_SIZE == len(return_value):
                 result = return_value[0]
                 error_msg = return_value[1]
             else:
