a0d283fcafd7250346a39f40baba2990a6870548
mikramarc <michal@shadowrobot.com>
2020-03-09 08:42:29 +0000

    fixing comment

--- sr_utilities_common/src/sr_utilities_common/ros_heartbeat.cpp
+++ sr_utilities_common/src/sr_utilities_common/ros_heartbeat.cpp
@@ -23,7 +23,8 @@ RosHeartbeat::RosHeartbeat(std::string heartbeat_topic_name, float heartbeat_tim
   ROS_INFO_STREAM(ros::this_node::getName() << ": initializing heartbeat...");
   heartbeat_topic_subscriber_ = nh_.subscribe(heartbeat_topic_name, 1,
                                               &RosHeartbeat::on_heartbeat_message_cb, this);
-  ros::Duration(1).sleep();  // let subscriber sort itself before starting the timer
+  // let subscriber sort itself before starting the timer
+  ros::Duration(1).sleep();
   heartbeat_timer_ = nh_.createTimer(heartbeat_timer_duration_, &RosHeartbeat::on_heartbeat_absent, this);
 }
 
