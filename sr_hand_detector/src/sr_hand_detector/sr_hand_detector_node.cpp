5da0010a38efaaf89d11632d57c15310044331dc
mikramarc <michal@shadowrobot.com>
2020-03-26 16:19:50 +0000

    minor change

--- sr_hand_detector/src/sr_hand_detector/sr_hand_detector_node.cpp
+++ sr_hand_detector/src/sr_hand_detector/sr_hand_detector_node.cpp
@@ -14,7 +14,6 @@ int main(int argc, char** argv)
   sr_hand_detector.get_port_names();
   for (int i=0; i<sr_hand_detector.num_ports_; i++)
   {
-      ROS_INFO_STREAM(sr_hand_detector.port_names_[i] << std::endl);
       ROS_INFO_STREAM(sr_hand_detector.count_slaves(i) << std::endl);
   }
   return 0;
