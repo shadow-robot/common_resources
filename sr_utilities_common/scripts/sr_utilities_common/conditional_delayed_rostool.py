8265e150461b486eb4212600ed8a27bbae9e21b4
giusebar <giuseppe@shadowrobot.com>
2020-04-29 18:27:59 +0000

    refactor

--- sr_utilities_common/scripts/sr_utilities_common/conditional_delayed_rostool.py
+++ sr_utilities_common/scripts/sr_utilities_common/conditional_delayed_rostool.py
@@ -20,35 +20,28 @@ import rosservice
 from multiprocessing import Process
 
 class TimeoutException(Exception):
-    def __init__(self, items_not_found):
-        self.items_not_found = items_not_found
+    def __init__(self):
+        pass
 
     def __str__(self):
-        return "TimeoutException {}".format(self.items_not_found)
+        return "TimeoutException"
 
-class RosConceptHandler(object):
-    def __init__(self, concept_type):
-        self._concept_type = concept_type
-        self._check_rate = rospy.Rate(10)
-        self._available_concepts = []
-        self._not_available_concepts = []
+class RosElementsHandler(object):
+    def __init__(self, element_type, required_elements_array):
+        self._element_type = element_type
+        self._required_elements_array = required_elements_array
+        self.missing_elements = []
 
-    def wait_for_concept(self, required_concepts_array, timeout=None):
-        all_concepts_available = False
-        time = rospy.Time.now() + timeout
-        while not all_concepts_available:
-            roscore_published_concepts = self._retrieve_available_concept(self._concept_type)
-            print("Av conc: ", roscore_published_concepts)
-            self._check_available_concepts(roscore_published_concepts, required_concepts_array)
-            if len(self._available_concepts) == len(required_concepts_array):
-                all_concepts_available = True
-            if (round(rospy.Time.now().to_sec(), 1) == round(time.to_sec(), 1)):
-                self._get_not_available_concepts(required_concepts_array)
-                raise TimeoutException(self._not_available_concepts)
-            self._check_rate.sleep()
-        return all_concepts_available
+    def execute(self):
+        all_elements_available = False
+        roscore_published_elements = self._retrieve_available_elements(self._element_type)
+        self._check_if_element_is_available(roscore_published_elements, self._required_elements_array)
+        if 0 == len(self._required_elements_array):
+            all_elements_available = True
+        self.missing_elements = self._required_elements_array
+        return all_elements_available
 
-    def _retrieve_available_concept(self, object_type):
+    def _retrieve_available_elements(self, object_type):
         if object_type == "topic":
             return rospy.get_published_topics()
         elif object_type == "service":
@@ -56,63 +49,56 @@ class RosConceptHandler(object):
         elif object_type == "param":
             return rospy.get_param_names()
 
-    def _check_available_concepts(self, roscore_published_concepts, required_concepts_array):
-        for concept in required_concepts_array:
-            if concept not in self._available_concepts:
-                if any(concept in sublist for sublist in roscore_published_concepts):
-                    rospy.loginfo("Found %s", concept)
-                    self._available_concepts.append(concept)
+    def _check_if_element_is_available(self, roscore_published_elements, required_elements_array):
+        for element in required_elements_array:
+            if any(element in sublist for sublist in roscore_published_elements):
+                rospy.loginfo("Found %s", element)
+                required_elements_array.remove(element)
 
-    def _get_not_available_concepts(self, required_topics_array):
-        for concept in required_topics_array:
-            if concept not in self._available_concepts:
-                self._not_available_concepts.append(concept)
-
-class ConditionalDelayedRosTool(object):
-    def __init__(self, package_name, executable_name, topics_array, params_array, services_array):
-        self._package_name = package_name
-        self._executable_name = executable_name
-        self._topics_array = topics_array
-        self._topic_handler = RosConceptHandler("topic")
-        self._params_array = params_array
-        self._param_handler = RosConceptHandler("param")
-        self._services_array = services_array
-        self._service_handler = RosConceptHandler("service")
-        self._timeout = rospy.Duration(1.0)
-
-    def _wait_for_conditions(self):
-        """
-        check result of the handlers
-        """
-        all_conditions_met = False
-
-        try:
-            wait_for_topic_process = Process(target=self._topic_handler.wait_for_concept(self._topics_array, self._timeout))
-            wait_for_topic_process.start()
-            wait_for_param_process = Process(target=self._param_handler.wait_for_concept(self._params_array, self._timeout))
-            wait_for_param_process.start()
-            wait_for_service_process = Process(target=self._service_handler.wait_for_concept(self._services_array, self._timeout))
-            wait_for_service_process.start()
-            wait_for_topic_process.join()
-            wait_for_param_process.join()
-            wait_for_service_process.join()
-        except TimeoutException as error:
-            rospy.logerr('Timeout error, could not find the following: %s', error.items_not_found)
-        else:
-            all_conditions_met = True
-
-        return all_conditions_met
-
-    def execute(self):
-        """
-        run if everything has been received
-        """
-        if self._wait_for_conditions():
-            rospy.loginfo("launching launchfile")
-        else:
-            rospy.logwarn("could not launch sorry :(")
 
 if __name__ == "__main__":
     rospy.init_node('conditional_delayed_roslaunch', anonymous=True)
-    cdlrt = ConditionalDelayedRosTool("cacca", "pupu", ["/rosout"], ["/rosdistro", "pu"], ["/rosout/get_loggers", "gesu"])
-    cdlrt.execute()
+
+    package_name = rospy.get_param("~package_name")
+    executable_name = rospy.get_param("~executable_name")
+    executable_type = rospy.get_param("~executable_type")
+    topics_array = rospy.get_param("~topics_array")
+    params_array = rospy.get_param("~params_array")
+    services_array = rospy.get_param("~services_array")
+    arguments_array = rospy.get_param("~arguments_array")
+    timeout = rospy.get_param("~timeout")
+    timeout = rospy.Duration(1.0)
+
+    topic_handler = RosElementsHandler("topic", topics_array)
+    param_handler = RosElementsHandler("param", params_array)
+    service_handler = RosElementsHandler("service", services_array)
+
+    time = rospy.Time.now() + timeout
+    all_conditions_satisfied = False
+    while not all_conditions_satisfied:
+        all_topics_available = topic_handler.execute()
+        all_params_available = param_handler.execute()
+        all_services_available = service_handler.execute()
+        if all_topics_available and all_params_available and all_services_available:
+            all_conditions_satisfied = True
+        if (round(rospy.Time.now().to_sec(), 1) == round(time.to_sec(), 1)):
+            try:
+                raise TimeoutException
+            except TimeoutException as e:
+                rospy.logerr("Timeout exceeded %s", e)
+                if topic_handler.missing_elements:
+                    rospy.logerr('Could not find the following topics: %s', topic_handler.missing_elements)
+                if param_handler.missing_elements:
+                    rospy.logerr('Could not find the following params: %s', param_handler.missing_elements)
+                if service_handler.missing_elements:
+                    rospy.logerr('Could not find the following sevices: %s', service_handler.missing_elements)
+            break
+
+    if all_conditions_satisfied:
+        if executable_type == "node":
+            os.system("rosrun {} {}")
+        elif executable_type == "launch":
+            print("roslaunch {} {} {}".format(package_name, executable_name, " ".join(arguments_array)))
+            os.system("roslaunch {} {} {}".format(package_name, executable_name, " ".join(arguments_array)))
+    else:
+        print("Could not launch bro")
