58d3bbbea0ccce9de2af522e5325a0ea24f615a8
carebare47 <tom@shadowrobot.com>
2020-09-14 11:18:57 +0000

    initial commit

---
+++ sr_utilities_common/scripts/sr_utilities_common/remote_power_control.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Copyright 2020 Shadow Robot Company Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import rospy
+import os
+import rosservice
+import sys
+
+
+
+
+class RemotePowerControl(object):
+    def __init__(self):
+
+
+
+if __name__ == "__main__":
+    rospy.init_node('remote_power_control', anonymous=True)
