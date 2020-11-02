6eed0e2df0aaaec1a8c924fe680a55c8b863ae68
giusebar <giuseppe@shadorobot.com>
2020-11-02 11:36:46 +0000

    fix lint

--- sr_utilities_common/include/sr_utilities_common/wait_for_param.h
+++ sr_utilities_common/include/sr_utilities_common/wait_for_param.h
@@ -1,12 +1,26 @@
+
 /*
-* Copyright (C) 2019 Shadow Robot Company Ltd - All Rights Reserved. Proprietary and Confidential.
-* Unauthorized copying of the content in this file, via any medium is strictly prohibited.
+* Copyright 2020 Shadow Robot Company Ltd.
+*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 #ifndef SR_UTILITIES_COMMON_WAIT_FOR_PARAM_H
 #define SR_UTILITIES_COMMON_WAIT_FOR_PARAM_H
 
 #include <ros/ros.h>
+#include <string>
 
 bool wait_for_param(ros::NodeHandle node_handle, std::string param_name, double timeout_in_secs = 0);
 
--- sr_utilities_common/src/sr_utilities_common/wait_for_param.cpp
+++ sr_utilities_common/src/sr_utilities_common/wait_for_param.cpp
@@ -1,4 +1,21 @@
+/*
+* Copyright 2020 Shadow Robot Company Ltd.
+*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <sr_utilities_common/wait_for_param.h>
+#include <string>
 
 bool wait_for_param(ros::NodeHandle node_handle, std::string param_name, double timeout_in_secs)
 {
