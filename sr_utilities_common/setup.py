f4b1fe976285e2bf0ffbae7b014731e218dcdb46
Georgia Blanco-Litchfield <georgia@shadowrobot.com>
2020-11-27 13:53:05 +0000

    Src 5244 wrist mimic rostest (#147)
    * adding heartbeat mock to sr_utilities_common
    
    * adding heartbeat mock to sr_utilities_common
    
    * adding heartbeat mock to sr_utilities_common
    
    * adding heartbeat publisher
    
    * adding heartbeat publisher
    
    * reviewer fixer
    
    * reviewer fixes
    
    * reviewer fixes
    
    * editing as per review
    
    * editing as per review

--- sr_utilities_common/setup.py
+++ sr_utilities_common/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['sr_utilities_common'],
     scripts=[],
-    package_dir={'': 'scripts'}
+    package_dir={'': 'scripts', '': 'src'}
 )
 
 setup(**d)
