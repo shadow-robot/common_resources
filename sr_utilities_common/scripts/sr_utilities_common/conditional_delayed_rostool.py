da82c185252f67796cb892bf4a1d1bcf21d15179
Tom Queen <tom_q@hotmail.co.uk>
2020-12-18 16:21:43 +0000

    F add default to launch arg list (#149)
    * add default so we don't have to specify launch_args_list
    
    * fixing
    
    * much cleaner implementation
    
    Co-authored-by: carebare47 <tom@shadowrobot.com>

--- sr_utilities_common/scripts/sr_utilities_common/conditional_delayed_rostool.py
+++ sr_utilities_common/scripts/sr_utilities_common/conditional_delayed_rostool.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
 
     package_name = rospy.get_param("~package_name")
     executable_name = rospy.get_param("~executable_name")
-    arguments_list = rospy.get_param("~launch_args_list")
+    arguments_list = rospy.get_param("~launch_args_list", "")
     timeout = rospy.get_param("~timeout", 0)
 
     if rospy.has_param('~topics_list'):
