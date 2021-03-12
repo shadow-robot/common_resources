24d208c83fdf6f474d2968d474696a1750eedca1
mikramarc <michal@shadowrobot.com>
2021-03-12 14:58:36 +0000

    SRC-5239: Adding speech control (#158)
    * Adding speech control
    
    * adding license
    
    * lint
    
    * fixing according to review
    
    * fixing lint
    
    Co-authored-by: mykolasjuraitis <75975027+mykolasjuraitis@users.noreply.github.com>

---
+++ sr_speech_control/README.md
@@ -0,0 +1,16 @@
+# sr_speech_control
+
+Package for controlling various systems using speech. Commands are being sent to a topic (default: `speech_control`) in a form of std_msgs String and can be intercepted by other nodes in order to execute actions.
+
+# Usage
+
+Create a `SpeechControl` class object and run it using the `run()` method. Depending on the trigger word that you have chosed, commands (from the commands list specified in the class constructor) proceeded by that word will be executed, and all other words and phrases will be ignored. E.g., if your trigger word is `Shadow` and you say `execute grasp`, no messages will be send to the topic. On the other hand, if you say `shadow grasp`, a `grasp` message will be sent.
+
+Example class implementation:
+
+```python
+    trigger_word = "shadow"
+    command_words = ["grasp", "release", "disable", "enable"]
+    sc = SpeechControl(trigger_word, command_words)
+    sc.run()
+```
---
+++ sr_speech_control/src/sr_speech_control/speech_control.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+# Copyright 2021 Shadow Robot Company Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import rospy
+import time
+import speech_recognition as sr
+from difflib import get_close_matches
+from std_msgs.msg import String
+
+
+class SpeechControl(object):
+    def __init__(self, trigger_word, command_words, command_topic='speech_control',
+                 non_speaking_duration=0.1, pause_threshold=0.2):
+        self.trigger_word = trigger_word
+        self.command_words = command_words
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
+        self.command_publisher = rospy.Publisher(command_topic, String, queue_size=1)
+        self.command_to_be_executed = None
+
+        self._init_recognizer(non_speaking_duration, pause_threshold)
+        self._stop_listening = self.recognizer.listen_in_background(self.microphone, self._recognizer_callback)
+
+    def _init_recognizer(self, non_speaking_duration, pause_threshold):
+        with self.microphone as source:
+            self.recognizer.adjust_for_ambient_noise(source)
+        self.recognizer.non_speaking_duration = non_speaking_duration
+        self.recognizer.pause_threshold = pause_threshold
+
+    def _recognizer_callback(self, recognizer, audio):
+        try:
+            result = recognizer.recognize_google(audio)
+        except sr.UnknownValueError:
+            return
+        except sr.RequestError as e:
+            rospy.logwarn("Could not request results from Google Speech Recognition service: {}".format(e))
+            return
+
+        result = [str(x).lower() for x in result.split(' ')]
+        if self._filter_word(result[0], self.trigger_word) == self.trigger_word:
+            command = self._filter_word(result[1], self.command_words)
+            if command in self.command_words:
+                self.command_to_be_executed = command
+
+    def _filter_word(self, word, dictionary, offset=0.5):
+        result = get_close_matches(word, dictionary, 1, offset)
+        if not result:
+            return word
+        return result[0]
+
+    def run(self):
+        rospy.loginfo("Started speech control. Trigger word: {}".format(self.trigger_word))
+        while not rospy.is_shutdown():
+                if self.command_to_be_executed:
+                    rospy.loginfo("Executing: {}.".format(self.command_to_be_executed))
+                    self.command_publisher.publish(self.command_to_be_executed)
+                    self.command_to_be_executed = None
+        self._stop_listening(wait_for_stop=False)
+
+
+if __name__ == "__main__":
+    rospy.init_node('example_speech_control', anonymous=True)
+
+    trigger_word = "shadow"
+    command_words = ["grasp", "release", "disable", "enable"]
+    sc = SpeechControl(trigger_word, command_words)
+    sc.run()
