3cc15b147d24b829322263cbd16451c23e716491
maxzieba <max@shadowrobot.com>
2021-03-25 21:12:55 +0000

    Improved logger

--- sr_wear_logger/src/sr_wear_logger.py
+++ sr_wear_logger/src/sr_wear_logger.py
@@ -39,42 +39,45 @@ class WearLogger():
         launch_file_path = ('/home/user/projects/shadow_robot/base/src'
                             '/common_resources/sr_wear_logger/launch/sr_wear_logger_launch.launch')
         cli_args = [launch_file_path, 'upload:=true']
-        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        uuid = roslaunch.rlutil.get_or_generate_uuid(None, True)
         roslaunch.configure_logging(uuid)
         roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], cli_args[1:])]
         parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
-        parent.spin_once()
+        parent.start()
         rospy.sleep(2)
         parent.shutdown()
         rospy.sleep(2)
+        
         return rospy.get_param('aws_upload_succeeded')
 
     def _downloadFromAWS(self):
         launch_file_path = ('/home/user/projects/shadow_robot/base/src'
                             '/common_resources/sr_wear_logger/launch/sr_wear_logger_launch.launch')
         cli_args = [launch_file_path, 'download:=true']
-        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        uuid = roslaunch.rlutil.get_or_generate_uuid(None, True)
         roslaunch.configure_logging(uuid)
         roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], cli_args[1:])]
         parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
-        parent.spin_once()
+        parent.start()
         rospy.sleep(2)
         parent.shutdown()
         rospy.sleep(2)
+
         return rospy.get_param('aws_download_succeeded')
 
     def _saveData(self):
         f = open(self.logFilePath+self.logFileName, 'w')
-        #self._verifyDataBeforeSave()
+        self._verifyDataBeforeSave()
         yaml.safe_dump(self.currentValues, f)
         f.close()
         print(self.currentValues)
-        print("File uploaded to AWS:" + str(self._uploadToAWS()))
+        print("LEN:",len(self.currentValues)) 
+        if len(self.currentValues) > 2:
+            print("File uploaded to AWS:" + str(self._uploadToAWS()))
 
     def __init__(self):
         rospy.init_node('sr_wear_logger_node')
-        rospy.on_shutdown(self._saveData)
-
+        
         self.first_run = True
         self.previousValues = {}
         self.currentValues = {}
@@ -87,6 +90,8 @@ class WearLogger():
         rospy.sleep(1)
         self.sub = rospy.Subscriber('/joint_states', JointState, self.callback)
 
+        rospy.on_shutdown(self._saveData)
+
     def _checkInternetConnection(self):
         connected = False
         url = "http://www.google.com"
@@ -98,6 +103,12 @@ class WearLogger():
             pass 
         return connected
 
+    def _verifyDataBeforeSave(self):
+        print("---VERIFICATION---")
+        print(self.currentValues)
+        print("---VERIFICATION END---")
+
+
     def _updateLog(self, fileName_1, fileName_2):
 
         f1 = open(fileName_1, 'r')
@@ -106,9 +117,10 @@ class WearLogger():
         l1 = yaml.load(f1)
         l2 = yaml.load(f2)
 
+        print(type(l1))
         print("###################################")
-        #for key in self.currentValues.keys():
-            #self.currentValues[key] = max(l1[key], l2[key])
+        for key in self.currentValues.keys():
+            self.currentValues[key] = max(l1[key], l2[key])
         print("###################################")
 
         f1.close()
@@ -116,22 +128,20 @@ class WearLogger():
 
 
     def _initLog(self):
-
         
         print("path exists?:"+str(os.path.exists(self.logFilePath)))
         if not os.path.exists(self.logFilePath):
-            os.makedirs(self.logFilePath)
             print("path doesn't exist. attempting to download from aws")
-            if not self._downloadFromAWS():
-                print("no file - aws failed")
+            #if not self._downloadFromAWS():
+                #print("no file - aws failed")
             
             
         if os.path.exists(self.logFilePath + self.logFileName):
             print("file exists - loading data")
 
             shutil.copyfile(self.logFilePath + self.logFileName, self.logFilePath + "/wear_data2.yaml")
-            if not self._downloadFromAWS():
-                print("failed there - aws failed")
+            #if not self._downloadFromAWS():
+               # print("filed there - aws failed")
             self._updateLog(self.logFilePath + self.logFileName, self.logFilePath + "/wear_data2.yaml")
 
             f = open(self.logFilePath+self.logFileName, 'r')
