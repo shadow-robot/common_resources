62972e01bb8fb2120c6b28f2af1b74256b7b71a9
maxzieba <max@shadowrobot.com>
2021-04-12 10:28:24 +0000

    Cleaning up code

--- sr_utilities_common/src/sr_utilities_common/aws_manager.py
+++ sr_utilities_common/src/sr_utilities_common/aws_manager.py
@@ -60,14 +60,7 @@ class AWS_Manager(object):
         )
 
     def get_bucket_structure(self, bucket_name, path=None):
-        if path:
-            print("Have path",path)
-            if self._client.list_objects(Bucket=bucket_name, Prefix=path).has_key('Contents'):
-                print("Have contents", self._client.list_objects(Bucket=bucket_name, Prefix=path))
-                return self._client.list_objects(Bucket=bucket_name, Prefix=path)['Contents']
-
-        print("AWS else:", self._client.list_objects(Bucket=bucket_name)['Contents'])
-        return self._client.list_objects(Bucket=bucket_name)['Contents']
+        return False, self._client.list_objects(Bucket=bucket_name, Prefix=path)['Contents']
 
     def _prepare_structure(self, bucket_name, files_base_path, files_folder_path, file_names):
         self.file_full_paths = []
