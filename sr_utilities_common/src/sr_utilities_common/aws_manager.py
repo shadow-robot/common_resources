b5479452819ac0cf8fcb369a86e4ed3803c615c5
maxzieba <77073326+maxzieba@users.noreply.github.com>
2021-05-06 13:05:58 +0200

    Implement 2 metrics to report MTBF (#164)
    * Adding wear logger
    
    * Adding wear logger
    
    * Added AWS handling
    
    * CI check
    
    * Removing unused files
    
    * Adding sample data
    
    * Modify package.xml
    
    * Updated package.xml
    
    * Updating sr_wear_logger
    
    * Improved logger
    
    * Update
    
    * Fixed issue with process monitor
    
    * Fix on node
    
    * Adding AWS_Manager class
    
    * Removed previous changes
    
    * Added time measurement
    
    * Added AWS_Manager class
    
    * Cleaned from print statements
    
    * lint check fix
    
    * Fixed saving of time value
    
    * Applied minor changes
    
    * Added license
    
    * Changed license
    
    * Changing license
    
    * Moving aws_manager to /src
    
    * Applied changes from comments
    
    * Added exception handling
    
    * Added exception handling
    
    * Applying some minor fixes
    
    * Lint check fixes
    
    * Adding function to get files listed in a bucket
    
    * Differentiate log files by serial number
    
    * Test launch file
    
    * Lint check fix
    
    * Cleaning up code
    
    * Fixing return value
    
    * Small changes
    
    * Minor changes
    
    * Changing default serial number
    
    * Lint checks
    
    * Lint checks
    
    * Appplied comment changes
    
    * Added README, CMakeLists and package.xml
    
    * Lint check
    
    * Fix package.xml
    
    * Applied changes
    
    * Added launch files with arguments
    
    * Removed bad launch file
    
    * Fixed wrong arg name
    
    * Small changes
    
    * Small fixes
    
    * Added manual test for aws_manager class
    
    * Lint check
    
    * Lint check
    
    * Correcting layout and adding exception to aws_manager
    
    * Added test for wear logger
    
    * Fix CMakeLists
    
    * Moved files
    
    * Minor fixes
    
    * Added dummy publisher for test
    
    * Fixed minor issue with test
    
    * Changed order of execution
    
    * Added dummy publisher to test
    
    * Lint check
    
    * Changed license
    
    * Added missing rostest dep
    
    * Fixing test, adding stop method to wear_logger
    
    * Added for safety
    
    * Lint check
    
    * Added hand prefix to launch file, divide init_log into smaller chunks
    
    * Lint check
    
    * Removing test files accordingly to pattern
    
    * Fixing wrong constructors for test cases
    
    * Applying comment changes, improving aws_manager
    
    * Lint checks
    
    * Cleaning up unused files after test
    
    * Removed loginfo for successful operation of upload/download
    
    * Lint check

---
+++ sr_utilities_common/src/sr_utilities_common/aws_manager.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+
+# Copyright 2020 Shadow Robot Company Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import rospy
+import logging
+import boto3
+from botocore.exceptions import *
+import subprocess
+import requests
+import re
+import json
+import os
+
+
+class AWS_Manager(object):
+    def __init__(self):
+
+        self.file_full_paths = []
+        self.aws_paths = []
+
+        aws_access_key_id = ""
+        aws_secret_access_key = ""
+        aws_session_token = ""
+
+        try:
+            with open('/usr/local/bin/customer.key', 'r') as customer_key_file:
+                customer_key = customer_key_file.read()
+        except:
+            rospy.logerr("Could not find customer key, ask software team for help!")
+        headers = {
+            'x-api-key': '{}'.format(customer_key[:-1]),
+        }
+
+        try:
+            response = requests.get('https://5vv2z6j3a7.execute-api.eu-west-2.amazonaws.com/prod', headers=headers)
+        except:
+            rospy.logerr("Could not request secret AWS access key, ask software team for help!")
+
+        if response is not None:
+            result = re.search('ACCESS_KEY_ID=(.*)\nSECRET_ACCESS', response.text)
+            aws_access_key_id = result.group(1)
+            result = re.search('SECRET_ACCESS_KEY=(.*)\nSESSION_TOKEN', response.text)
+            aws_secret_access_key = result.group(1)
+            result = re.search('SESSION_TOKEN=(.*)\nEXPIRATION', response.text)
+            aws_session_token = result.group(1)
+
+        self._client = boto3.client(
+            's3',
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            aws_session_token=aws_session_token
+        )
+
+    def get_bucket_structure_with_prefix(self, bucket_name, prefix):
+        if prefix:
+            try:
+                if 'Contents' in self._client.list_objects(Bucket=bucket_name, Prefix=prefix):
+                    return self._client.list_objects(Bucket=bucket_name, Prefix=prefix)['Contents']
+            except Exception as e:
+                rospy.logwarn("Failed listing bucket objects. " + str(e))
+        return None
+
+    def _prepare_structure(self, bucket_name, files_base_path, files_folder_path, file_names):
+        self.file_full_paths = []
+        self.aws_paths = []
+        for file_name in file_names:
+            self.file_full_paths.append("{}/{}/{}".format(files_base_path, files_folder_path, file_name))
+            self.aws_paths.append("{}/{}".format(files_folder_path, file_name))
+
+    def download(self, bucket_name, files_base_path, files_folder_path, file_names):
+        self._prepare_structure(bucket_name, files_base_path, files_folder_path, file_names)
+        directory = "{}/{}".format(files_base_path, files_folder_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        downloadSucceded = False
+        for self.file_full_path, self.aws_path in zip(self.file_full_paths, self.aws_paths):
+            try:
+                self._client.download_file(bucket_name, self.aws_path, self.file_full_path)
+                downloadSucceded = True
+            except Exception as e:
+                rospy.logwarn("File download failed. " + str(e))
+        return downloadSucceded
+
+    def upload(self, bucket_name, files_base_path, files_folder_path, file_names):
+        self._prepare_structure(bucket_name, files_base_path, files_folder_path, file_names)
+        uploadSucceded = False
+        for self.file_full_path, self.aws_path in zip(self.file_full_paths, self.aws_paths):
+            try:
+                self._client.upload_file(self.file_full_path, bucket_name, self.aws_path)
+                uploadSucceded = True
+            except Exception as e:
+                rospy.loginfo("File upload failed" + str(e))
+        return uploadSucceded
+
+
+if __name__ == "__main__":
+    rospy.init_node("aws_manager_node")
+
+    download_param = rospy.get_param("~download")
+    upload_param = rospy.get_param("~upload")
+    bucket_name = rospy.get_param("~bucket_name")
+    files_base_path = rospy.get_param("~files_base_path")
+    files_folder_path = rospy.get_param("~files_folder_path")
+    file_names = rospy.get_param("~file_names")
+
+    aws_manager = AWS_Manager()
+
+    if upload_param is True:
+        status_msg = "File upload failed"
+        rospy.loginfo("Uploading {} file.".format(self.aws_path))
+        if aws_manager.download(bucket_name, files_base_path, files_folder_path, file_names):
+            status_msg = "Completed file upload."
+        rospy.loginfo(status_msg)
+
+    if download_param is True:
+        status_msg = "File download failed"
+        rospy.loginfo("Downloading {} file.".format(self.aws_path))
+        if aws_manager.upload(bucket_name, files_base_path, files_folder_path, file_names):
+            status_msg = "Completed file download."
+        rospy.loginfo(status_msg)
+
+    rospy.signal_shutdown("")
