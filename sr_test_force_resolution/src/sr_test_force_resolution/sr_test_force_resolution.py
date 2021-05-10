5055b1a5cbfe1a4bfc9ed18ef87bc51d7d8563ac
carebare47 <tom@shadowrobot.com>
2021-05-10 16:36:04 +0000

    now saves output as CSV

--- sr_test_force_resolution/src/sr_test_force_resolution/sr_test_force_resolution.py
+++ sr_test_force_resolution/src/sr_test_force_resolution/sr_test_force_resolution.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
+# -*- coding: latin-1 -*-
 
 import rospy
 import copy
+import termios
+import sys
+import csv
+import datetime
+from pynput import keyboard
 from control_msgs.msg import JointControllerState
 from sensor_msgs.msg import JointState
 from sr_robot_commander.sr_hand_commander import SrHandCommander
@@ -31,6 +37,13 @@ class ControllerStateMonitor():
         self.process_value = 0
         self.process_value_dot = 0
         self.sub = []
+        self.output_controller_state_keys = ['timestamp', 'command', 'setpoint', 'process_value', 'process_value_dot']
+        self.output_controller_state = {}
+        self.initialise_output_dictionary()
+
+    def initialise_output_dictionary(self):
+        for key in self.output_controller_state_keys:
+            self.output_controller_state[key] = []
 
     def unsubscribe_all(self):
         for subscriber in self.sub:
@@ -38,10 +51,18 @@ class ControllerStateMonitor():
 
 
 class TestForceResolution():
-    def __init__(self):
+    def __init__(self, keyboard_control=True):
         self.active_tests = []
         self._controller_subscribers = {}
         self._last_joint_state = JointState()
+        self._ctrl_pressed = False
+        self._shift_pressed = False
+        self._alt_pressed = False
+        self._kb_listener = None
+        self._output_joint_state = {}
+        self._output_controller_state = {}
+        self._output_jointstate_keys = ['timestamp', 'position', 'velocity', 'effort']
+        self._output_jointstate = {}
         self.requested_joints = ['FFJ3', 'FFJ4', 'MFJ3', 'MFJ4', 'RFJ3', 'RFJ4', 'LFJ3', 'LFJ4']
         self._joint_states_zero = {'rh_FFJ1': 0, 'rh_FFJ2': 0, 'rh_FFJ3': 0, 'rh_FFJ4': 0,
                                    'rh_MFJ1': 0, 'rh_MFJ2': 0, 'rh_MFJ3': 0, 'rh_MFJ4': 0,
@@ -75,8 +96,43 @@ class TestForceResolution():
         self.joint_state_subscriber = rospy.Subscriber('/joint_states', JointState, self.joint_state_cb)
         self.hand_commander = SrHandCommander(name="right_hand")
         self.hand_commander.move_to_joint_value_target(self._joint_states_zero, wait=True, angle_degrees=True)
+        self.initialise_output_dictionary()
         self.setup_controller_subscribers()
-        self.perform_tests()
+        # self.perform_tests()
+        while not rospy.is_shutdown():
+            self.run()
+
+
+    def initialise_output_dictionary(self):
+        for key in self._output_jointstate_keys:
+            self._output_jointstate[key] = []
+
+    def clear_output_dictionary(self):
+        for key, value in self._output_jointstate.iteritems():
+            value = []
+
+    def run(self):
+        CONST_EXIT_CHAR = 'Q'
+        CONST_ALL_CHAR = 'A'
+        in_string = ""
+        while in_string not in self.requested_joints:
+                print "Available joints: ", self.requested_joints
+                in_string = raw_input("Please enter joint ('q' to exit, 'a' for all):").upper()
+                if CONST_EXIT_CHAR.lower() == in_string.lower():
+                    sys.exit(0)
+                if CONST_ALL_CHAR.lower() == in_string.lower():
+                    rospy.loginfo("running all")
+                    for joint in self.requested_joints:
+                        self.run_joint(joint)
+                if in_string not in self.requested_joints and in_string not in CONST_EXIT_CHAR and in_string not in CONST_ALL_CHAR:
+                    rospy.logwarn("%s is not a valid joint", in_string)
+        self.run_joint(in_string)
+
+    def run_joint(self, in_string):
+        self.go_to_zero_joint_state()
+        self.test_joint(in_string)
+        self.go_to_zero_joint_state()
+
 
     def perform_tests(self):
         self.go_to_zero_joint_state()
@@ -85,12 +141,16 @@ class TestForceResolution():
             self.go_to_zero_joint_state()
 
     def free_j4(self, joint):
+        print
+        rospy.loginfo("Making some space around %s", joint)
         which_finger = joint.split('J')[0]
+        which_joint = joint.split('J')[1]
         self._clear_j4[which_finger]
         for key, value in self._clear_j4[which_finger].iteritems():
-            rospy.loginfo("moving %s to %s", key + 'J4', value)
+            rospy.loginfo("moving %s to %s (%s)", key + 'J4', value, self._joint_ranges[which_joint][which_finger][value])
             self.move_joint_minmax(key + 'J4', value, wait=False)
         self.move_joint_angle(joint, 0, wait=True)
+        print
 
     def all_minmax(self, minmax='min', wait=False):
         for joint in self.requested_joints:
@@ -99,25 +159,49 @@ class TestForceResolution():
     def test_joint(self, joint):
         if '4' in joint:
             self.free_j4(joint)
+        rospy.loginfo("Testing joint %s", joint)
         self.activate_output(joint, True)
         self.move_joint_minmax(joint, 'min')
         self.move_joint_minmax(joint, 'max')
         self.move_joint_angle(joint, 0)
         self.activate_output(joint, False)
+        # self.print_dict(self._controller_subscribers[joint].output_controller_state)
+        # self.print_dict(self._output_jointstate)
+        self.write_output_dictionaries(joint)
+        print
+        print
+
+
+    def write_output_dictionaries(self, joint):
+        filename = self.construct_filename(joint)
+        self.write_output_jointstate_dictionary(filename)
+        self.write_output_controllerstate_dictionary(filename, joint)
+
+
+    def print_dict(self, dictionary):
+        for key, value in dictionary.iteritems():
+            print key, value
 
     def activate_output(self, joint, enable):
-        self._controller_subscribers[joint].enable_output = enable
         if enable:
+            self._controller_subscribers[joint].initialise_output_dictionary()
+            self.initialise_output_dictionary()
             self.active_tests.append(joint)
         else:
             self.active_tests = []
+        self._controller_subscribers[joint].enable_output = enable
 
     def print_joint_state(self, joint):
         idx = self._last_joint_state.name.index("rh_" + joint)
         position = self._last_joint_state.position[idx]
         velocity = self._last_joint_state.velocity[idx]
         effort = self._last_joint_state.effort[idx]
-        rospy.loginfo("pos: %f vel: %f eff: %f", position, velocity, effort)
+        timestamp = self._last_joint_state.header.stamp
+        # rospy.loginfo("pos: %f vel: %f eff: %f", position, velocity, effort)
+        self._output_jointstate['timestamp'].append(timestamp)
+        self._output_jointstate['position'].append(position)
+        self._output_jointstate['velocity'].append(velocity)
+        self._output_jointstate['effort'].append(effort)
 
     def go_to_zero_joint_state(self):
         self.hand_commander.move_to_joint_value_target(self._joint_states_zero, wait=True, angle_degrees=True)
@@ -129,7 +213,6 @@ class TestForceResolution():
         # rospy.loginfo("which_finger: %s  joint_number: %s", which_finger, joint_number)
         # target_joint_states["rh_" + joint.upper()] = self._m_joint_ranges[joint_number][which_finger][min_max]
         target_joint_states["rh_" + joint.upper()] = self._m_joint_ranges[joint_number][min_max]
-        print target_joint_states
         self.hand_commander.move_to_joint_value_target(target_joint_states, wait=wait, angle_degrees=True)
 
     def move_joint_angle(self, joint, angle, wait=True):
@@ -147,14 +230,22 @@ class TestForceResolution():
             controller_state_monitor.process_value = msg.process_value
             controller_state_monitor.process_value_dot = msg.process_value_dot
             controller_state_monitor.new_data = True
+            keyt = key
             if controller_state_monitor.enable_output:
-                rospy.loginfo("name   command  setpoint  process  proc_dot")
-                rospy.loginfo("%s: %f %f %f %f",
-                              controller_state_monitor.name,
-                              controller_state_monitor.command,
-                              controller_state_monitor.set_point,
-                              controller_state_monitor.process_value,
-                              controller_state_monitor.process_value_dot)
+                # rospy.loginfo("name   command  setpoint  process  proc_dot")
+                # rospy.loginfo("%s: %f %f %f %f",
+                #               controller_state_monitor.name,
+                #               controller_state_monitor.command,
+                #               controller_state_monitor.set_point,
+                #               controller_state_monitor.process_value,
+                #               controller_state_monitor.process_value_dot)
+                # rospy.loginfo("key1: %s key2: %s cmd: %s", keyt, controller_state_monitor.name, str(msg.command))
+                controller_state_monitor.output_controller_state['command'].append(msg.command)
+                controller_state_monitor.output_controller_state['timestamp'].append(msg.header.stamp)
+                controller_state_monitor.output_controller_state['setpoint'].append(msg.set_point)
+                controller_state_monitor.output_controller_state['process_value'].append(msg.process_value)
+                controller_state_monitor.output_controller_state['process_value_dot'].append(msg.process_value_dot)
+
 
         controller_state_monitor.sub.append(rospy.Subscriber(topic_name, JointControllerState, controller_subscriber))
         return controller_state_monitor
@@ -173,8 +264,60 @@ class TestForceResolution():
         if self.active_tests:
             self.print_joint_state(self.active_tests[0])
 
+    def construct_filename(self, joint):
+        day = datetime.datetime.now().day
+        month = datetime.datetime.now().month
+        year = datetime.datetime.now().year
+        hour = datetime.datetime.now().hour
+        minute = datetime.datetime.now().minute
+        second = datetime.datetime.now().second
+        return joint + '_' + str(year) + '_' + str(month) + '_' + str(day) + '_' + str(hour) + '_' + str(minute) + '_' + str(second) + '.csv'
+
+    def write_output_jointstate_dictionary(self, filename):
+        output_filename = 'jointstate_' + filename
+        fieldnames = []
+        rows = []
+        for key in self._output_jointstate_keys:
+            fieldnames.append(key)
+        length_of_list = len(self._output_jointstate[self._output_jointstate_keys[0]])
+        rospy.loginfo("Collected %s jointstate datapoints", str(length_of_list))
+        for i in range(0, length_of_list):
+            line = {}
+            for key in self._output_jointstate_keys:
+                line[key] = self._output_jointstate[key][i]
+            rows.append(line)
+        self.write_csv(output_filename, fieldnames, rows)
+
+
+    def write_output_controllerstate_dictionary(self, filename, joint):
+        output_filename = 'controllerstate_' + filename
+        fieldnames = []
+        rows = []
+        keys = self._controller_subscribers[joint].output_controller_state_keys
+        for key in keys:
+            fieldnames.append(key)
+        length_of_list = len(self._controller_subscribers[joint].output_controller_state[keys[0]])
+        rospy.loginfo("Collected %s controllerstate datapoints", str(length_of_list))
+        for i in range(0, length_of_list):
+            line = {}
+            for key in keys:
+                line[key] = self._controller_subscribers[joint].output_controller_state[key][i]
+            rows.append(line)
+        self.write_csv(output_filename, fieldnames, rows)
+
+    def write_csv(self, filename, fieldnames, rows):
+        with open(filename, mode='w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in rows:
+                writer.writerow(row)
+        rospy.loginfo("file: %s saved", filename)
+
 
 if __name__ == '__main__':
     rospy.init_node("sr_test_force_resolution")
     tfr = TestForceResolution()
     # tfr.free_j4('FFJ4')
+
+
+
