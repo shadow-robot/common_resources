dfbd6e75310f22fce5a8195f27fb5363758c0716
maxzieba <max@shadowrobot.com>
2021-08-02 16:06:34 +0000

    Workg toward multiple devices

--- sr_finger_mount/src/test2.py
+++ sr_finger_mount/src/test2.py
@@ -8,7 +8,7 @@ import sounddevice as sd
 import matplotlib.pyplot as plt
 from sr_robot_msgs.msg import ShadowPST
 from std_msgs.msg import Float32
-
+import threading
 
 def int_or_str(text):
     """Helper function for argument parsing."""
@@ -67,7 +67,16 @@ collection_2 = []
 fc = 10
 #amplitude = amp[0]
 oldsignal = 0
+start_idx = 0
+thr_name = ""
+# Every device has 2 channels 
+# 
+class DevHandler():
+    def __init__(self, device, fingers):
+        self.device_name = device
+        self.fingers = fingers
 
+    
 class SrFingerMount():
     def __init__(self, fingers, hand_id = "rh"):
         self._used_fingers = fingers
@@ -76,7 +85,8 @@ class SrFingerMount():
         self._pst_min = 350
         self._mapped_pst_values = dict()
 
-        self._device_name = "HDA"
+        self._acceptable_device_names = ["Boreas DevKit", "BOS1901-KIT"]
+        self._used_devices = []
 
         self.sub = rospy.Subscriber("/"+hand_id+"/tactile", ShadowPST, self.tactile_cb)
 
@@ -91,42 +101,77 @@ class SrFingerMount():
         if set(self._used_fingers).intersection(set(self._fingers)):
             pass
         else:
-            print(self._used_fingers," and ",self._fingers)
             rospy.logwarn("Verify used fingers!")
             exit(0)
 
         self.check_devices()
+        self.init_all()
 
     def check_devices(self):
+
         needed_devices = math.ceil(len(self._used_fingers)/2)
-
         device_list = sd.query_devices()
-
         present_devices = 0
-        for device in device_list:
-            if self._device_name in device['name']:
-                present_devices += 1
-        
-        if needed_devices != present_devices:
+
+        for acceptable_device in self._acceptable_device_names:
+            for device in device_list:
+                if acceptable_device in device['name']:
+                    self._used_devices.append(device['name'])
+                    present_devices += 1
+
+        if needed_devices > present_devices:
             rospy.logerr("Not enough dev kits ({}/{}) connected to handle {} fingers".format(present_devices,needed_devices, len(self._used_fingers)))
-            exit(0)
+            exit(0)         
+        else:
+            rospy.logerr("Using {} devices [{}]".format(len(self._used_devices), self._used_devices))   
 
+    def callback(self, outdata, frames, time, status):
+        rospy.logwarn(thr_name)
+        if status:
+            print(status, file=sys.stderr)
+        global start_idx
+        t = (start_idx + np.arange(frames)) / 8000 # samplerate
+        t = t.reshape(-1, 1)
 
-    def start_piezo(self):
-        samplerate = sd.query_devices(args.device, 'output')['default_samplerate']
+        outdata[:] = args.amplitude/2 * np.sin(2 * np.pi * args.frequency * t) + args.amplitude/2
 
-        with sd.OutputStream(device=args.device, channels=len(self._used_fingers), callback=callback,
-                         samplerate=samplerate):
+        rospy.logwarn("prinitn")
+        rospy.logwarn(type(outdata))
+        rospy.logwarn(outdata.shape)
+        rospy.logwarn(type(t))
+        rospy.logwarn(t.shape)
+        #rospy.logwarn(outdata)
+
+        start_idx += frames
+    
+
+    def _sublists(self):
+        channels_per_device = 2
+        for i in range(0, len(self._used_fingers), channels_per_device): 
+            yield self._used_fingers[i:i + channels_per_device]
+
+    def start_piezo(self, device_name, fingers):         
+        thr_name = str(fingers)       
+        with sd.OutputStream(device=device_name, channels=len(fingers), callback=self.callback,
+                         samplerate=8000):
             print('#' * 80)
             print('press Return to quit')
             print('#' * 80)
             input()
-            plt.subplot(2,1,1)
-            plt.plot(collection_1)
-            plt.subplot(2,1,2)
-            plt.plot(collection_2)
-            plt.show()
 
+    def init_all(self):
+        x = [None, None]
+        finger_sets = list(self._sublists())
+        for i, finger_set in enumerate(finger_sets):
+            device_name = self._used_devices[i]
+            rospy.logwarn(i)
+            rospy.logwarn(device_name)
+            rospy.logwarn(finger_set)
+
+            samplerate = sd.query_devices(device_name, 'output')['default_samplerate']
+            rospy.logerr(samplerate)
+            x[i] = threading.Thread(target=self.start_piezo, args=(device_name,finger_set))
+            x[i].start()
 
     def tactile_cb(self, data):        
         if len(data.pressure) == 5:
@@ -142,112 +187,9 @@ class SrFingerMount():
         
 
 rospy.init_node("sr_finger_mount_test")
-f_1 = SrFingerMount(["th", "ff", "mf", "rf", "lf"])
-f_1 = SrFingerMount(["ff", "mf"])
-f_2 = SrFingerMount(["rf", "lf"])
-f_3 = SrFingerMount(["th"])
+f_1 = SrFingerMount(["th", "ff"])
 
 
 while not rospy.is_shutdown():
     rospy.spin()
     rospy.Rate(10).sleep()
-'''
-    
-try:
-    samplerate = sd.query_devices(args.device, 'output')['default_samplerate']
-
-    rospy.init_node("sr_finger_mount_test")
-    samplerate = sd.query_devices(args.device, 'output')['default_samplerate']
-
-    rospy.Subscriber("/rh/tactile", ShadowPST, tactile_cb)
-    rospy.wait_for_message("/rh/tactile", ShadowPST)
-    
-    def callback(outdata, frames, time, status):
-        if status:
-            print(status, file=sys.stderr)
-        global m_phase1, m_phase2, collection_1, collection_2
-        global fm, fc, amplitude, oldsignal
-        global amp, amphead, amphead_inc, amphead_dec
-        global freq, freqhead, freqhead_inc, freqhead_dec
-        newfrequency1 = freq[freqhead]
-        #newamplitude = amp[amphead]
-        
-        #Am = 0.4
-        Ac = 0.4
-        #fm = 0
-        
-        signal = np.zeros(frames)
-        signal_2 = np.zeros(frames)
-        for i in range(frames):
-            phaseInc1 = 2*np.pi*fm/samplerate
-            phaseInc2 = 2*np.pi*fc/samplerate
-            signal[i] = (Ac + Am * np.sin(m_phase1)) * np.sin(m_phase2)
-            signal_2[i] = (Ac + Am * np.sin(m_phase1+5)) * np.sin(m_phase2-3)
-            m_phase1 += phaseInc1
-            m_phase2 += phaseInc2
-            
-            #print(i, amplitude, fm, newamplitude, newfrequency, signal[i], oldsignal)
-            if ((signal[i] <= 0) and (oldsignal >= 0) or (signal[i] >= 0) and (oldsignal <= 0)):
-                fc = newfrequency1
-                #Am = newamplitude1
-                
-            oldsignal = signal[i]
-            
-        collection_1 += list(signal)
-        collection_2 += list(signal_2)
-        #collection += list([2])
-        #t = (m_phase + np.arange(frames)) / samplerate
-        #t = t.reshape(-1, 1)
-        signal = signal.reshape(-1, 384)
-        signal_2 = signal_2.reshape(-1, 384)
-        #print(signal.shape)
-        #print(outdata.shape)
-        #signal = amplitude * np.sin(2 * np.pi * f1 * t)
-        outdata[:,0] = signal
-        outdata[:,1] = signal_2
-        print(outdata.shape)
-        #outdata[:,1] = signal_2
-
-        #m_phase += frames        
-        #print(t)        
-        
-        if amphead == 99:
-            amphead_dec = True
-            amphead_inc = False
-        elif amphead == 0:
-            amphead_inc = True
-            amphead_dec = False
-        if amphead_dec:
-            amphead -= 1
-        if amphead_inc:
-            amphead += 1
-            
-        if freqhead == 99:
-            freqhead_dec = True
-            freqhead_inc = False
-        elif freqhead == 0:
-            freqhead_inc = True
-            freqhead_dec = False
-        if freqhead_dec:
-            freqhead -= 1
-        if freqhead_inc:
-            freqhead += 1
-        #print(frames)
-
-    with sd.OutputStream(device=args.device, channels=2, callback=callback,
-                         samplerate=samplerate):
-        print('#' * 80)
-        print('press Return to quit')
-        print('#' * 80)
-        input()
-        plt.subplot(2,1,1)
-        plt.plot(collection_1)
-        plt.subplot(2,1,2)
-        plt.plot(collection_2)
-        plt.show()
-except KeyboardInterrupt:
-    parser.exit('')
-except Exception as e:
-    print(e)
-    parser.exit(type(e).__name__ + ': ' + str(e))
-'''
