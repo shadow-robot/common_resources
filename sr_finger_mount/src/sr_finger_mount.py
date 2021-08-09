c833e8b0f650a5551b68b46752243a65a1c54a3a
maxzieba <max@shadowrobot.com>
2021-08-09 13:22:09 +0000

    Clean up

--- sr_finger_mount/src/sr_finger_mount.py
+++ sr_finger_mount/src/sr_finger_mount.py
@@ -12,37 +12,6 @@ import threading
 import time
 import copy
 
-def int_or_str(text):
-    """Helper function for argument parsing."""
-    try:
-        return int(text)
-    except ValueError:
-        return text
-
-
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit')
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    'frequency', nargs='?', metavar='FREQUENCY', type=float, default=500,
-    help='frequency in Hz (default: %(default)s)')
-parser.add_argument(
-    '-d', '--device', type=int_or_str,
-    help='output device (numeric ID or substring)')
-parser.add_argument(
-    '-a', '--amplitude', type=float, default=0.2,
-    help='amplitude (default: %(default)s)')
-args = parser.parse_args(remaining)
-
 
 # Every device has 2 channels 
 class DeviceHandler(threading.Thread):
@@ -58,17 +27,19 @@ class DeviceHandler(threading.Thread):
         self.amp = [1] * len(self.fingers)
 
     def run(self):
+        # for loop only for plotting 
         for f in self.fingers:
             self.mount._collection[f] = list()
+
         self.start_piezo(self.fingers)
 
     def callback(self, outdata, frames, time, status):
         if status:
             print(status, file=sys.stderr)
-        
+
         for i, finger in enumerate(self.fingers):
-                 
-            t = (self.start_idx[i] + np.arange(frames)) / self.samplerate 
+
+            t = (self.start_idx[i] + np.arange(frames)) / self.samplerate
             t = t.reshape(-1, 1)
             self.start_idx[i] += frames
 
@@ -76,33 +47,22 @@ class DeviceHandler(threading.Thread):
                 phaseInc = 2*np.pi*self.freq[i]/self.samplerate
                 outdata[frame,i] = self.amp[i] * np.sin(self.m_phase[i]) #+ amp/2
                 self.m_phase[i] += phaseInc
-                #rospy.logwarn("Current frequency {} and {}".format(freq, phaseInc))
 
                 if np.sign(outdata[frame,i]) != np.sign(self.oldsignal[i]):
                     self.freq[i] = self.mount._fm[finger]
                     self.amp[i] = self.mount._am[finger]
                 self.oldsignal[i] = outdata[frame,i]
-            
-            self.mount._collection[finger].extend(outdata[:,i])
-        
-            
-        '''
-        t = (self.start_idx[0] + np.arange(frames)) / self.samplerate 
-        t = t.reshape(-1, 1)
-        self.start_idx[0] += frames
-        outdata[:] = list(self.mount._am['th'] * np.sin(2*np.pi*self.mount._fm['th']*t))
-        #collection += list(outdata[:,0])
-        '''
-        
 
-    def start_piezo(self, fingers):       
-        self.samplerate = sd.query_devices(self.device_name, 'output')['default_samplerate']  
+            # only for plotting
+            self.mount._collection[finger].extend(outdata[:,i])
+
+    def start_piezo(self, fingers):
+        self.samplerate = sd.query_devices(self.device_name, 'output')['default_samplerate']
         with sd.OutputStream(device=self.device_name, channels=len(fingers), callback=self.callback,
-                         samplerate=self.samplerate):
+                             samplerate=self.samplerate):
             #while not rospy.is_shutdown():
             #    continue
 
-            #global collection 
             input("Press anything to finish...")
             for i, f in enumerate(fingers):
                 plt.subplot(len(fingers), 1, i+1)
@@ -114,14 +74,13 @@ class SrFingerMount():
     def __init__(self, fingers, hand_id = "rh"):
         self._used_fingers = fingers
         self._fingers = ["th", "ff", "mf", "rf", "lf"]
-        self._pst_max = 200 #950
-        self._pst_min = 1 #350
-        self._mapped_pst_values = dict()
+        self._pst_max = 200 # 950
+        self._pst_min = 1 # 350
 
         self._acceptable_device_names = ["Boreas DevKit", "BOS1901-KIT"]
         self._used_devices = []
 
-        self.sub = rospy.Subscriber("/"+hand_id+"/tactile", ShadowPST, self.tactile_cb)
+        self.sub = rospy.Subscriber("/"+hand_id+"/tactile", ShadowPST, self._tactile_cb)
 
         self._amp_max = 1
         self._amp_min = 0.2
@@ -130,32 +89,30 @@ class SrFingerMount():
 
         self._am = dict()
         self._fm = dict()
+
+        # ONLY FOR PLOTTING PURPOSES
         self._collection = dict(zip(fingers, list(len(fingers)*list())))
-        rospy.logwarn("Collection")
-        rospy.logwarn(self._collection)
 
-        if set(self._used_fingers).intersection(set(self._fingers)):
-            pass
-        else:
+        if not set(self._used_fingers).intersection(set(self._fingers)):
             rospy.logwarn("Verify used fingers!")
-            exit(0)
+            return 
 
-        self.check_devices()
-        self.init_all()
+        if self.check_devices():
+            self.init_all()
 
-    def tactile_cb(self, data):        
+    def _tactile_cb(self, data):
         if len(data.pressure) == 5:
-            self._mapped_pst_values = dict(zip(self._fingers, data.pressure))
+            mapped_pst_values = dict(zip(self._fingers, data.pressure))
 
             for f in self._used_fingers:
-                self._am[f] = ((self._mapped_pst_values[f] - self._pst_min) / (self._pst_max - self._pst_min))* \
-                                (self._amp_max - self._amp_min) + self._amp_min
-                self._fm[f] = ((self._mapped_pst_values[f] - self._pst_min) / (self._pst_max - self._pst_min))* \
-                                (self._freq_max - self._freq_min) + self._freq_min
+                self._am[f] = ((mapped_pst_values[f] - self._pst_min) / (self._pst_max - self._pst_min))* \
+                               (self._amp_max - self._amp_min) + self._amp_min
+                self._fm[f] = ((mapped_pst_values[f] - self._pst_min) / (self._pst_max - self._pst_min))* \
+                               (self._freq_max - self._freq_min) + self._freq_min
         else:
             rospy.logwarn("Missing values")
 
-    # checks if there are enough devices to handle the given amount of fingers 
+    # checks if there are enough devices to handle the given amount of fingers
     def check_devices(self):
         needed_devices = math.ceil(len(self._used_fingers)/2)
         device_list = sd.query_devices()
@@ -169,13 +126,13 @@ class SrFingerMount():
 
         if needed_devices > present_devices:
             rospy.logerr("Not enough dev kits ({}/{}) connected to handle {} fingers".format(present_devices,needed_devices, len(self._used_fingers)))
-            exit(0)         
-        else:
-            rospy.logerr("Using {} devices [{}]".format(len(self._used_devices), self._used_devices))   
+            return False
+
+        return True
 
     def _sublists(self):
         channels_per_device = 2
-        for i in range(0, len(self._used_fingers), channels_per_device): 
+        for i in range(0, len(self._used_fingers), channels_per_device):
             yield self._used_fingers[i:i + channels_per_device]
 
     def init_all(self):
@@ -190,11 +147,38 @@ class SrFingerMount():
             time.sleep(1)
             x[i].start()
 
+if __name__ == "__main__":
 
-rospy.init_node("sr_finger_mount_test")
-f_1 = SrFingerMount(["th", "ff", "mf"])
+    def int_or_str(text):
+        """Helper function for argument parsing."""
+        try:
+            return int(text)
+        except ValueError:
+            return text
 
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument(
+        '-l', '--list-devices', action='store_true',
+        help='show list of audio devices and exit')
+    args, remaining = parser.parse_known_args()
+    if args.list_devices:
+        print(sd.query_devices())
+        parser.exit(0)
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        parents=[parser])
+    parser.add_argument(
+        'frequency', nargs='?', metavar='FREQUENCY', type=float, default=500,
+        help='frequency in Hz (default: %(default)s)')
+    parser.add_argument(
+        '-d', '--device', type=int_or_str,
+        help='output device (numeric ID or substring)')
+    parser.add_argument(
+        '-a', '--amplitude', type=float, default=0.2,
+        help='amplitude (default: %(default)s)')
+    args = parser.parse_args(remaining)
 
-while not rospy.is_shutdown():
-    rospy.spin()
-    rospy.Rate(10).sleep()
+    rospy.init_node("sr_finger_mount_test")
+    mount = SrFingerMount(["ff"])
+
