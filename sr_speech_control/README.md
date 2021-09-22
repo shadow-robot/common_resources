4251d9567cf2e744b319229b605ba8b809e1733d
maxzieba <77073326+maxzieba@users.noreply.github.com>
2021-09-22 12:08:49 +0100

    SRC-6704  Add voice feedback to voice controller (#199)
    * voice test
    
    * Improvements
    
    * Adding forked pydub
    
    * Fixing index position arg
    
    * Update repository.rosinstall
    
    * Adding new words
    
    * Adding device selection - default Logitech
    
    * Lint fixes
    
    * Updated README
    
    * Moving param to next line
    
    * Fixing param name
    
    * Update README and package.xml
    
    * Change format
    
    * Improving fail safety
    
    * Updating README, rosinstall and package.xml
    
    * Delete .rosinstall
    
    * Applying comment changes
    
    * Updating README
    
    * Update README.md
    
    * Updated package.xml
    
    * Adding pydub presence condition for voice feedback
    
    * Updated README
    
    * Applying comments
    
    * Remove empty lines
    
    * Applying comment changes
    
    * Applying comment changes
    
    * Changing logging level
    
    * Changing to correct error message

--- sr_speech_control/README.md
+++ sr_speech_control/README.md
@@ -4,6 +4,14 @@ A node for controlling various systems using speech.
 Commands are being sent to a chosen topic in a form of std_msgs
 String and can be intercepted by other nodes in order to execute actions.
 
+## Requirements
+
+In order to use the voice feedback it is obligatory to install the related audio libary. For this purpose we are using forked [PyDub](https://github.com/jiaaro/pydub/tree/master/pydub) with custom changes (output device selection) which can be installed by running:
+
+```
+pip install git+https://github.com/shadow-robot/pydub@output_selection
+```
+
 ## Usage
 
 To start the node run:
@@ -14,20 +22,23 @@ rosrun sr_speech_control speech_control.py
 The node will permanenly listen to the microphone input, will use Google speech
 recognition to translate audio to text, check if text starts with a chosen trigger word and if so, will publish word after the trigger word to a `sr_speech_control` topic (by default) .
 
-The class used has four parameters that can be provided in order to modify the behaviour:
+The class used has five parameters that can be provided in order to modify the behaviour:
 - `trigger_word` - sets a word that preceeds a command to be sent
-- `command_words` - list of commands that are allowed to be sent
+- `command_words_and_feedback` - dictionary of commands that are allowed to be sent and corresponding sound feedback words
 - `similar_words_dict_path` - path to a yaml file containing dictionary of words that are easily mistaken for a trigger word or one of the command words
 - `non_speaking_duration` - seconds of non-speaking audio to keep on both sides of the recording
 - `pause_threshold` - seconds of non-speaking audio before a phrase is considered complete
+- `output_device_name` - the device name on which the voice feedback confirmation should be played. Most probably this will be the devices brand or model (to verify use `lsusb`).
 
 An example usage can be seen in the `speech_control.py` file:
 ```python
     trigger_word = "shadow"
-    command_words = ["grasp", "release", "disable", "enable", "engage"]
+    command_words_and_feedback = {"grasp": "grasped", "release": "released", "disable": "disabled",                   
+                                  "enable": "enabled", "engage": "engaged", "open": "opened"}
     similar_words_dict_path = rospkg.RosPack().get_path('sr_speech_control') + '/config/similar_words_dict.yaml'
 
-    sc = SpeechControl(trigger_word, command_words, similar_words_dict_path=similar_words_dict_path)
+    sc = SpeechControl(trigger_word, command_words_and_feedback, similar_words_dict_path=similar_words_dict_path,
+                       output_device_name="Logitech")
     sc.run()
 
 ```
--- sr_speech_control/src/sr_speech_control/speech_control.py
+++ sr_speech_control/src/sr_speech_control/speech_control.py
@@ -25,10 +25,24 @@ from std_msgs.msg import String
 import yaml
 from builtins import input
 
+from gtts import gTTS
+from io import BytesIO
+import pyaudio
+
+try:
+    from pydub import AudioSegment
+    from pydub.playback import play
+except (ImportError, ModuleNotFoundError) as e:
+    rospy.logerr("Custom PyDub is not installed. No voice feedback.")
+    rospy.logerr("Install by running pip install git+https://github.com/shadow-robot/pydub@output_selection")
+    raise
+
 
 class SpeechControl(object):
     def __init__(self, trigger_word, command_words, command_topic='sr_speech_control',
-                 similar_words_dict_path=None, non_speaking_duration=0.2, pause_threshold=0.2):
+                 similar_words_dict_path=None, non_speaking_duration=0.2, pause_threshold=0.2,
+                 output_device_name=None):
+
         self.trigger_word = trigger_word
         self.command_words = command_words
         self.recognizer = sr.Recognizer()
@@ -40,6 +54,27 @@ class SpeechControl(object):
             self.parse_similar_words_dict(similar_words_dict_path)
         self._init_recognizer(non_speaking_duration, pause_threshold)
         self._stop_listening = self.recognizer.listen_in_background(self.microphone, self._recognizer_callback)
+
+        self.output_device_index, self.output_device_sample_rate = self.get_output_device_data(output_device_name)
+
+    @staticmethod
+    def get_output_device_data(device_name=None):
+        p = pyaudio.PyAudio()
+        output_device_index = None
+        output_device_rate = None
+
+        try:
+            output_device_index = p.get_default_output_device_info()['index']
+            output_device_rate = p.get_default_output_device_info()['defaultSampleRate']
+            if device_name:
+                for index in range(0, p.get_device_count()):
+                    if device_name in p.get_device_info_by_index(index)['name']:
+                        output_device_index = index
+                        output_device_rate = p.get_device_info_by_index(index)['defaultSampleRate']
+        except IOError:
+            rospy.logwarn("Selected device not found. Using first available output device")
+
+        return output_device_index, output_device_rate
 
     def parse_similar_words_dict(self, path_name):
         with open(path_name, 'r') as stream:
@@ -76,9 +111,11 @@ class SpeechControl(object):
             return
 
         result = [str(x).lower() for x in result.split(' ')]
+        rospy.loginfo("Received: {}".format(result))
 
         if self._filter_word(result[0], self.trigger_word) == self.trigger_word:
             command = self._filter_word(''.join(result[1:]), self.command_words)
+            rospy.loginfo("Understood as : {}".format(command))
             if command in self.command_words:
                 self.command_to_be_executed = command
 
@@ -91,22 +128,40 @@ class SpeechControl(object):
             return word
         return result[0]
 
+    @staticmethod
+    def confirm_voice_command(command, output_device_index=None, sample_rate=None):
+        language = 'en'
+        tts = gTTS(text=command, lang=language, slow=False)
+        fp = BytesIO()
+        tts.write_to_fp(fp)
+        fp.seek(0)
+        audio = AudioSegment.from_file(fp, format="mp3")
+        if output_device_index is not None and sample_rate is not None:
+            audio = audio.set_frame_rate(int(sample_rate))
+        play(audio, output_device_index)
+
     def run(self):
         rospy.loginfo("Started speech control. Trigger word: {}".format(self.trigger_word))
         while not rospy.is_shutdown():
             if self.command_to_be_executed:
                 rospy.loginfo("Executing: {}.".format(self.command_to_be_executed))
                 self.command_publisher.publish(self.command_to_be_executed)
+                self.confirm_voice_command(self.command_words[self.command_to_be_executed],
+                                           self.output_device_index, self.output_device_sample_rate)
                 self.command_to_be_executed = None
         self._stop_listening(wait_for_stop=False)
 
 
 if __name__ == "__main__":
+
     rospy.init_node('example_speech_control', anonymous=True)
 
     trigger_word = "shadow"
-    command_words = ["grasp", "release", "disable", "enable", "engage"]
+    command_words_and_feedback = {"grasp": "grasped", "release": "released", "disable": "disabled",
+                                  "enable": "enabled", "engage": "engaged", "disengage": "disengaged",
+                                  "open": "opened"}
     similar_words_dict_path = rospkg.RosPack().get_path('sr_speech_control') + '/config/similar_words_dict.yaml'
 
-    sc = SpeechControl(trigger_word, command_words, similar_words_dict_path=similar_words_dict_path)
+    sc = SpeechControl(trigger_word, command_words_and_feedback, similar_words_dict_path=similar_words_dict_path,
+                       output_device_name="Logitech")
     sc.run()
