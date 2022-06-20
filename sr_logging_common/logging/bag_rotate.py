8e68a6e1cd81d750c154df749493d04a2a37ea28
Ben <86227623+BenStarmerSmith@users.noreply.github.com>
2022-06-20 10:36:27 +0100

    Fix broken rosbags (#234)
    * Added new code to bag_rotate to get active rosbags and fix them.
    
    * Changed to fixed imports, changed some variable names, reduced number of if statments used.
    
    * Fixed the script to actually work.
    
    * Removed test print.
    
    * Added checks for files before removing them. Added the path to the filename.
    
    * Removed duplicate code to get rid of active bags.
    
    * Added defaults to rosparams in python file. Changed default path to .ros/log
    
    * Reverted default launch file param.
    
    * Removed from future.
    
    * Added back from future (tmp) and removed unused const.

--- sr_logging_common/logging/bag_rotate.py
+++ sr_logging_common/logging/bag_rotate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2019 Shadow Robot Company Ltd.
+# Copyright 2019, 2022 Shadow Robot Company Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
@@ -15,34 +15,55 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
-import os
+from os import listdir, remove
+from os.path import getctime, join, exists
+import sys
 import time
 import rospy
+import subprocess
 
 
 def remover(desired_bag_number, path):
-    CONST_MAX_ACTIVE_BAGS_ALLOWED = desired_bag_number//2
-
     while not rospy.is_shutdown():
-        bag_files = [f for f in os.listdir(path) if f.endswith('.bag')]
-        active_bag_files = [f for f in os.listdir(path) if f.endswith('.bag.active')]
+        bag_files = [bagfile for bagfile in listdir(path) if bagfile.endswith('.bag')]
 
-        sorted_bag_files = sorted(bag_files, key=lambda x: os.path.getctime(os.path.join(path, x)))
-        sorted_active_bag_files = sorted(active_bag_files, key=lambda x: os.path.getctime(os.path.join(path, x)))
+        sorted_bag_files = sorted(bag_files, key=lambda x: getctime(join(path, x)))
 
         while len(sorted_bag_files) > desired_bag_number:
-            os.remove(os.path.join(path, sorted_bag_files[0]))
+            remove(join(path, sorted_bag_files[0]))
             sorted_bag_files.pop(0)
-
-        while len(sorted_active_bag_files) > CONST_MAX_ACTIVE_BAGS_ALLOWED:
-            os.remove(os.path.join(path, sorted_active_bag_files[0]))
-            sorted_active_bag_files.pop(0)
 
         time.sleep(1)
 
 
+def gather_and_fix_all_active_rosbag_files(path):
+    active_rosbags = [join(path, bagfile) for bagfile in listdir(path) if bagfile.endswith('.bag.active')]
+    for bag_file in active_rosbags:
+        file_name = bag_file.split(".active")[0]
+        process = subprocess.run(["rosbag", "reindex", bag_file], capture_output=True, text=True)
+        if process.returncode != 0:
+            rospy.logerr(f"\nCOMMAND REINDEX:\nstdout: {process.stdout}\nstderr: {process.stderr}")
+        process = subprocess.run(["rosbag", "fix", bag_file, file_name], capture_output=True, text=True)
+        if process.returncode != 0:
+            rospy.logerr(f"\nCOMMAND FIX:\nstdout: {process.stdout}\nstderr: {process.stderr}")
+        if exists(file_name + ".active"):
+            remove(file_name + ".active")
+        if exists(file_name + ".orig.active"):
+            remove(file_name + ".orig.active")
+
+    if len(active_rosbags) > 0:
+        string = "Fixed the rosbag files:"
+        for filename in active_rosbags:
+            string += f"  {filename}"
+        rospy.loginfo(string)
+
+
 if __name__ == '__main__':
     rospy.init_node('bag_rotate', anonymous=True)
-    desired_bag_number = rospy.get_param('~bag_files_num')
-    path = rospy.get_param('~bag_files_path')
+    desired_bag_number = rospy.get_param('~bag_files_num', 6)
+    path = rospy.get_param('~bag_files_path', '/home/user/.ros/log')
+    if not exists(path):
+        rospy.logerr(f"Path {path} cannot be found.")
+        sys.exit(1)
+    gather_and_fix_all_active_rosbag_files(path)
     remover(desired_bag_number, path)
