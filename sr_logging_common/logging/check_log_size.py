b0f7deb1b0e4752bc730d5720b7b737e726eadd8
niko-holmes <holmesnikolaus@gmail.com>
2024-12-02 12:31:10 +0000

    better var naming + param descriptions

--- sr_logging_common/logging/check_log_size.py
+++ sr_logging_common/logging/check_log_size.py
@@ -22,20 +22,27 @@ import logging_utils as utils
 if __name__ == '__main__':
     rospy.init_node('check_log_size')
 
-    additional_required_disk_space = rospy.get_param('~additional_required_disk_space', 10) * utils.GIGABYTE
+    # -----------------------------------------Definition of parameters-------------------------------------------------
+    # non_log_headroom: Additional disk space allocated for non log file growth during a driver run.
+    # allocated_log_space: Minimum disk space allocated for roslogs. Logs are allowed to grow beyond this size.
+    # log_growth_headroom: Additional disk space allocated for log growth during a run. This is added to the current
+    #                      log size or allocated_log_space whichever is greater.
+    # ------------------------------------------------------------------------------------------------------------------
+
+    non_log_headroom = rospy.get_param('~additional_required_disk_space', 10) * utils.GIGABYTE
     allocated_log_space = rospy.get_param('~allocated_log_space', 10) * utils.GIGABYTE
     log_growth_headroom = rospy.get_param('~log_growth_headroom', 5) * utils.GIGABYTE
 
     total_disk_space, _, free_disk_space = shutil.disk_usage(utils.LOG_PATH)
     logs_size = utils.get_directory_size(utils.LOG_PATH)
 
-    if free_disk_space < log_growth_headroom + max(logs_size, allocated_log_space) + additional_required_disk_space:
-        required_disk_space = log_growth_headroom + max(logs_size, allocated_log_space) + additional_required_disk_space
+    if free_disk_space < log_growth_headroom + max(logs_size, allocated_log_space) + non_log_headroom:
+        required_disk_space = log_growth_headroom + max(logs_size, allocated_log_space) + non_log_headroom
         rospy.logfatal("Not enough free disk space to safely store roslogs:\n" +
                        f"\tFree Disk Space: {free_disk_space / utils.GIGABYTE:.3f} of"
                        f" {total_disk_space / utils.GIGABYTE:.3f} GB free\n" +
-                       f"\tRequired Free Disk Space: {required_disk_space / utils.GIGABYTE:.3f} GB\n\t\tMinimum" +
-                       f" Required Free Disk Space: {additional_required_disk_space / utils.GIGABYTE:.3f} GB\n" +
+                       f"\tRequired Free Disk Space: {required_disk_space / utils.GIGABYTE:.3f} GB\n" +
+                       f"\t\tNon-log Headroom: {non_log_headroom / utils.GIGABYTE:.3f} GB\n" +
                        f"\t\tAllocated Log Space: {allocated_log_space  / utils.GIGABYTE:.3f} GB\n" +
                        f"\t\tLog Growth Headroom: {log_growth_headroom  / utils.GIGABYTE:.3f} GB\n" +
                        f"\t\tCurrent Log Size: {logs_size / utils.GIGABYTE:.3f} GB")
