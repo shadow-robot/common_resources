70010053b6b8c558f3817def8f2593db0044fe54
Nikolaus Holmes <143398822+niko-holmes@users.noreply.github.com>
2024-12-11 15:28:28 +0000

    Core dump limit expects a directory size of 0 when the direcotyr does not exist. This functionality has been added back in

--- sr_logging_common/logging/core_dump_limit.py
+++ sr_logging_common/logging/core_dump_limit.py
@@ -25,7 +25,11 @@ if __name__ == '__main__':
     desired_size = rospy.get_param('~desired_folder_size', utils.GIGABYTE)
     path = rospy.get_param('~core_dump_path', utils.CORE_DUMPS_PATH)
     while not rospy.is_shutdown():
-        if utils.get_directory_size(path) > desired_size:
+        try:
+            directory_size = utils.get_directory_size(path)
+        except (FileNotFoundError, NotADirectoryError):
+            directory_size = 0
+        if directory_size > desired_size:
             oldest, _ = utils.get_oldest_item_in_directory(path)
             rospy.loginfo("Core dump size greater than limit. Removing oldest file: " + oldest)
             os.remove(os.path.join(path, oldest))
